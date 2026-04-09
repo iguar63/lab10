7c62e07fb58b94163f1974a9e16433b6d09d67d4
iguar63 <gdlgjhlkjklj@gmail.com>
2026-04-09 13:42:31 +0300

    dadada

--- app/src/main/java/com/example/lab10/MainActivity.kt
+++ app/src/main/java/com/example/lab10/MainActivity.kt
@@ -1,28 +1,93 @@
 package com.example.lab10
 
-import android.app.Activity
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.lab10.databinding.ActivityMainBinding
-    class MainActivity : AppCompatActivity() {
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import kotlin.properties.Delegates.notNull
+class MainActivity : AppCompatActivity() {
         lateinit var binding: ActivityMainBinding
+
+        private var g by notNull<Boolean>()
+        var kr  = false
+        var sob = false
+        var cot = false
+        var ptic = false
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+            if (savedInstanceState == null){
+                g = false
+            }else{
+                g = savedInstanceState.getBoolean(KEY_IS_VISIBLE)
+            }
+
             binding.zpshka.setOnClickListener {
-                if(binding.zpshka.isChecked){
-                    binding.hochet.visibility = View.INVISIBLE
-                }
+                if(binding.zpshka.isChecked){g = false
+                    render()}
             }
             binding.volonter.setOnClickListener {
-                if (binding.volonter.isChecked) {
-                    binding.hochet.visibility = View.VISIBLE
+                if (binding.volonter.isChecked) {g = true
+                render()}
+            }
+
+
+            binding.sob.setOnClickListener {
+                if (binding.sob.isChecked){
+                    sob = true
                 }
             }
+
+            binding.kosh.setOnClickListener {
+                if (binding.kosh.isChecked){
+                    cot = true
+                }
+            }
+
+            binding.krol.setOnClickListener {
+                if (binding.krol.isChecked){
+                    kr = true
+                }
+            }
+
+            binding.ptic.setOnClickListener {
+                if (binding.ptic.isChecked){
+                    ptic = true
+                }
+            }
+
+            if (ptic == true){
+                binding.ptic.isChecked
+            }
+
+            if (cot == true){
+                binding.kosh.isChecked
+            }
+
+            if (sob == true){
+                binding.sob.isChecked
+            }
+
+            if (kr == true){
+                binding.krol.isChecked
+            }
         }
+
+    private fun render() = with(binding){
+        hochet.visibility = if (g) View.VISIBLE else View.INVISIBLE
     }
+
+        override fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            outState.putBoolean(KEY_IS_VISIBLE,g)
+        }
+
+    companion object{
+        @JvmStatic private val KEY_IS_VISIBLE = "IS_VISIBLE"
+    }
+    }
+
 
